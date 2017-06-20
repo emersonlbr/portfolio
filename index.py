abb14c05ffb7f050f03d86616565884f1ff87926
emersonlbr <emersonlbr@icloud.com>
2017-06-20 19:25:51 -0300

    new files

---
+++ index.py
@@ -0,0 +1,14 @@
+print ("emerson is a fucking bitch")
+
+emerson = 4
+
+def lol():
+	if emerson > 1:
+		print(emerson)
+	else:
+		print("im not your negro")
+
+
+lol()
+
+#I think python's simplicity is really what makes me like this language so much. 
